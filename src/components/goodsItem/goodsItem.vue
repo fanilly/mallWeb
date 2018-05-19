6c5a47f513bc68afcd8a32300b1c17198b26dda3
fanilly <815969707@qq.com>
2018-05-19 10:08:26 +0800

    '初步完善goodsItem组件'

--- src/components/goodsItem/goodsItem.vue
+++ src/components/goodsItem/goodsItem.vue
@@ -6,15 +6,27 @@
     <section class="goods-item-rside">
       <h3 class="goods-item-rside-title">{{ item.goodsname }}</h3>
       <p class="goods-item-rside-info">{{ item.goodsname }}</p>
-      <section class="goods-item-rside-footer">
-
-      </section>
+      <div class="goods-item-rside-control-box">
+        <template v-if="item.count>=1">
+          <span class="icon-box reduce" @click="reduce()"><i class="iconfont icon-iconjian"></i></span>
+          <span class="count-box">{{item.count}}</span>
+        </template>
+        <span class="icon-box plus" @click="plus()"><i class="iconfont icon-iconjia"></i></span>
+      </div>
     </section>
   </section>
 </template>
 <script>
 export default {
   props: {
+    parentIndex: {
+      type: Number,
+      require: true
+    },
+    currentIndex: {
+      type: Number,
+      require: true
+    },
     baseurl: {
       type: String,
       default: ''
@@ -27,12 +39,29 @@ export default {
   data() {
     return {};
   },
-  mounted(){
-    // console.log(this.item);
+  methods: {
+    plus() {
+      this.$emit('plus',{
+        parentIndex:this.parentIndex,
+        currentIndex:this.currentIndex,
+        goodsId:this.item.goodsid
+      });
+    },
+    reduce() {
+      this.$emit('reduce',{
+        parentIndex:this.parentIndex,
+        currentIndex:this.currentIndex,
+        goodsId:this.item.goodsid
+      });
+    }
+  },
+  mounted() {
+
   }
 };
 
 </script>
 <style lang="less" scoped>
-  @import './goodsItem.less';
+@import './goodsItem.less';
+
 </style>
--- src/pages/Shop/shop.vue
+++ src/pages/Shop/shop.vue
@@ -4,14 +4,32 @@
       <section class="search-box">
         <section class="search-box-content">请输入关键词搜索商品</section>
       </section>
-      <section class="shop-info">
+      <section class="mall-info">
+        <section class="mall-info-header">
+          <section class="mall-info-header-lside">
+            <img v-lazy="configs.goodsimg" alt="店铺头像">
+          </section>
+          <section class="mall-info-header-rside">
+            <h2 class="mall-name">{{configs.mallName}}</h2>
+            <p class="mall-desc">{{configs.mallDesc}}</p>
+          </section>
+        </section>
       </section>
     </header>
     <main class="main">
       <scrolltab>
-        <scrolltab-panel :label="item.catname" :icon="item.icon || ''" v-for="item,key in classList" :key="key">
+        <scrolltab-panel :label="item.catname" :icon="item.icon || ''" v-for="item,index in classList" :key="index">
           <section class="class-container">
-            <goods-item v-for="(sub, key) in item.goodsList" :key="key" :item="sub" :baseurl="BASE_URL"></goods-item>
+            <goods-item
+              v-for="(sub, key) in item.goodsList"
+              :key="key"
+              :item="sub"
+              :baseurl="BASE_URL"
+              :parentIndex="index"
+              :currentIndex="key"
+              @plus="plus"
+              @reduce="reduce"
+            ></goods-item>
           </section>
         </scrolltab-panel>
       </scrolltab>
@@ -23,6 +41,7 @@ import scrolltabPanel from '@/components/scrolltab/scrolltab-panel.vue';
 import scrolltab from '@/components/scrolltab/scrolltab.vue';
 import goodsItem from '@/components/goodsItem/goodsItem.vue';
 import { BASE_URL } from '@/api/urls.js';
+import { setStorage, getStorage, removeStorage } from '@/utils/Storage.js';
 import {
   getGoosList
 } from '@/api/index.js';
@@ -32,14 +51,38 @@ export default {
   name: 'Shop',
   data() {
     return {
-      BASE_URL:BASE_URL,
-      classList: []
+      BASE_URL: BASE_URL,
+      classList: [],
+      configs:{}
     };
   },
+  methods:{
+    //购物车添加
+    plus(options){
+      this.classList[options.parentIndex].goodsList[options.currentIndex].count++;
+    },
+    //购物车减少
+    reduce(options){
+      this.classList[options.parentIndex].goodsList[options.currentIndex].count--;
+    }
+  },
   mounted() {
+    let trolley = getStorage('trolley');
+    console.log(trolley);
     getGoosList().then(res => {
       console.log(res);
-      this.classList = res.data.goods;
+      //记录商品数据
+      this.classList = res.data.goods.map(item => {
+        let tempItem = item;
+        tempItem.goodsList.map(goods => {
+          let tempGoods = goods;
+          tempGoods.count = 0;
+          return tempGoods;
+        });
+        return tempItem;
+      });
+      //记录配置数据
+      this.configs = res.data.configs;
     }).catch(err => {
       console.log(err);
     });
